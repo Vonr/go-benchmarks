d6c13358a09e76dc7c188cd9f6d38693e101b19c
Qther <dtaclanlegion@gmail.com>
2021-10-21 19:34:37 +0800

    Somewhat speedup FastAtan though still slower than math.Atan

--- trig/trig_test.go
+++ trig/trig_test.go
@@ -14,67 +14,59 @@ const (
 )
 
 func BenchmarkStdSine(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		math.Sin(float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		math.Sin(i)
 	}
 }
 
 func BenchmarkFastSine(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		FastSin(float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		FastSin(i)
 	}
 }
 
 func BenchmarkStdCos(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		math.Cos(float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		math.Cos(i)
 	}
 }
 
 func BenchmarkFastCos(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		FastCos(float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		FastCos(i)
 	}
 }
 
 func BenchmarkStdAtan2(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		math.Atan2(float64(b.N), float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		math.Atan2(i, i)
 	}
 }
 
 func BenchmarkFastAtan2(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		FastAtan2(float64(b.N), float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		FastAtan2(i, i)
 	}
 }
 
 func BenchmarkStdAtan(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		math.Atan(float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		math.Atan(i)
 	}
 }
 
 func BenchmarkFastAtan(b *testing.B) {
-	for i := 0; i < 10000000; i++ {
-		FastAtan(float64(b.N))
+	for i := 0.0; i < float64(b.N); i++ {
+		FastAtan(i)
 	}
 }
 
-func TestFastSin(t *testing.T) {
+func TestFastSinCos(t *testing.T) {
 	maxError := 0.0
 	for i := 0.0; i < Tau; i += 0.0000001 {
 		maxError = math.Max(maxError, math.Abs(FastSin(i)-math.Sin(i)))
 	}
-	fmt.Println("FastSin Max Error:", maxError)
-}
-
-func TestFastCos(t *testing.T) {
-	maxError := 0.0
-	for i := 0.0; i < Tau; i += 0.0000001 {
-		maxError = math.Max(maxError, math.Abs(FastCos(i)-math.Cos(i)))
-	}
-	fmt.Println("FastCos Max Error:", maxError)
+	fmt.Println("FastSin/Cos Max Error:", maxError)
 }
 
 func TestFastAtan2(t *testing.T) {
@@ -92,7 +84,7 @@ func TestFastAtan(t *testing.T) {
 	for i := 0.0; i < Tau; i += 0.0000001 {
 		maxError = math.Max(maxError, math.Abs(FastAtan(i)-math.Atan(i)))
 	}
-	fmt.Println("FastAtan Max Error:", maxError)
+	fmt.Println("FastAtan", maxError)
 }
 
 func FastSin(radians float64) float64 {
@@ -136,6 +128,27 @@ func FastAtan2(y float64, x float64) float64 {
 	return t3
 }
 
-func FastAtan(radians float64) float64 {
-	return FastAtan2(radians, 1.0)
+func FastAtan(y float64) float64 {
+	t1 := math.Abs(y)
+	t0 := math.Max(1, t1)
+	t1 = math.Min(1, t1)
+	t1 = t1 / t0
+
+	t4 := t1 * t1
+	t0 = -0.013480470
+	t0 = math.FMA(t0, t4, 0.057477314)
+	t0 = math.FMA(t0, t4, -0.121239071)
+	t0 = math.FMA(t0, t4, 0.195635925)
+	t0 = math.FMA(t0, t4, -0.332994597)
+	t0 = math.FMA(t0, t4, 0.999995630)
+	t1 = t0 * t1
+
+	if math.Abs(y) > 1.0 {
+		t1 = math.FMA(Pi, 0.5, -t1)
+	}
+
+	if y < 0 {
+		return -t1
+	}
+	return t1
 }
